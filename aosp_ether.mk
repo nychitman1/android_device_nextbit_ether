a5c501542ec4e629d0d6d2b97be0c6cacf5b4554
Josh Chasky <jchasky@gmail.com>
2016-08-11 23:12:41 -0400

    Ether: DUify a little further to go back to the traditional style

---
+++ aosp_ether.mk
@@ -0,0 +1,37 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Get the long list of APNs
+#PRODUCT_COPY_FILES := device/lge/bullhead/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+PRODUCT_NAME := aosp_ether
+PRODUCT_DEVICE := ether
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on Robin
+PRODUCT_MANUFACTURER := Nextbit
+
+$(call inherit-product, device/nextbit/ether/device.mk)
+$(call inherit-product-if-exists, vendor/nextbit/ether/ether-vendor.mk)
+
+PRODUCT_PACKAGES += \
+    Launcher3
--- du.mk
+++ du.mk
@@ -1,14 +1,24 @@
-# Inherit 64-bit configs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit device configuration
+$(call inherit-product, device/nextbit/ether/aosp_ether.mk)
 
 # Inherit some common DU stuff.
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/nextbit/ether/device.mk)
 $(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
@@ -16,7 +26,6 @@ PRODUCT_NAME := du_ether
 BOARD_VENDOR := nextbit
 TARGET_VENDOR := nextbit
 PRODUCT_DEVICE := ether
-
 PRODUCT_BRAND := Nextbit
 PRODUCT_MODEL := Robin
 PRODUCT_MANUFACTURER := Nextbit
