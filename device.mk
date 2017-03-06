01ddf4eec14ba93875ef57ead9770e832adf037b
Josh Chasky <jchasky@gmail.com>
2017-03-06 08:19:01 -0500

    Ether: Fix dev options

--- device.mk
+++ device.mk
@@ -149,8 +149,8 @@ PRODUCT_PACKAGES += fingerprintd
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/config
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.frp.pst=/dev/block/bootdevice/by-name/config
 
 # Lights
 PRODUCT_PACKAGES += \
